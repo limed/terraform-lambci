e801bb151cafa2962580d0bc973fd450b8c4da66
limed <limed@mozilla.com>
2017-03-02 16:32:35 -0800

    Adding some of the roles that this lambda function needs

--- main.tf
+++ main.tf
@@ -9,10 +9,18 @@ resource aws_lambda_function "lambci-function" {
   handler       = "index.handler"
   memory_size   = "1536"
   timeout       = "300"
-
+  role          = "${aws_iam_role.LambdaExecution.arn}"
   s3_bucket     = "lambci-${var.aws_region}"
   s3_key        = "fn/lambci-build-${var.lambci_version}.zip"
   runtime       = "nodejs4.3"
+}
+
+resource aws_s3_bucket "BuildResults" {
+  bucket  = "${var.lambci_instance}-buildresults"
+
+  tags {
+    Name  = "${var.lambci_instance}-buildresults"
+  }
 }
 
 resource aws_iam_role "LambdaExecution" {
@@ -27,7 +35,7 @@ resource aws_iam_role "LambdaExecution" {
     command = "sleep 30"
   }
 
-  assume_policy_role = <<EOF
+  assume_role_policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -45,5 +53,64 @@ resource aws_iam_role "LambdaExecution" {
     ]
 }
 EOF
-
 }
+
+resource aws_iam_role_policy "WriteLogs" {
+  name  = "WriteLogs"
+  role  = "${aws_iam_role.LambdaExecution.id}"
+
+  policy = <<EOF
+{
+  "Statement": {
+    "Action":[
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ],
+    "Resource":"arn:aws:logs:*:*:log-group:/aws/lambda/${var.lambci_instance}-*",
+    "Effect":"Allow"
+  }
+}
+EOF
+}
+
+resource aws_iam_role_policy "ReadWriteBucket" {
+  name  = "ReadWriteBucket"
+  role  = "${aws_iam_role.LambdaExecution.id}"
+
+  policy = <<EOF
+{
+  "Statement": {
+    "Action":[
+      "s3:GetObject",
+      "s3:PutObject",
+      "s3:PutObjectAcl"
+    ],
+    "Resource":"arn:aws:s3:::${aws_s3_bucket.BuildResults.id}/*",
+    "Effect":"Allow"
+  }
+}
+EOF
+}
+
+resource aws_iam_role_policy "ReadTables" {
+  name  = "ReadTables"
+  role  = "${aws_iam_role.LambdaExecution.id}"
+
+  policy = <<EOF
+{
+  "Statement": {
+    "Action": [
+      "dynamodb:BatchGetItem",
+      "dynamodb:GetItem",
+      "dynamodb:Query",
+      "dynamodb:Scan"
+    ],
+    "Resource":"arn:aws:dynamodb:*:*:table/${var.lambci_instance}-*",
+    "Effect":"Allow"
+  }
+}
+
+EOF
+}
+
